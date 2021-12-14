2c01aae1a024bc2cd2e37e827c15a4ad495f2040
jablonski.pio <piotr.jablonski@bcf-software.com>
2021-12-14 21:58:16 +0100

    Init commit

---
+++ src/main/java/com/gogoaren/indarra/serviceweatherapi/ServiceWeatherApiApplication.java
@@ -0,0 +1,13 @@
+package com.gogoaren.indarra.serviceweatherapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceWeatherApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceWeatherApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gogoaren/indarra/serviceweatherapi/ServiceWeatherApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gogoaren.indarra.serviceweatherapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceWeatherApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
