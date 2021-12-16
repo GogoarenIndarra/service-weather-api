c5703ca13328a341fc70ac4416ae390a19df72a2
jablonski.pio <piotr.jablonski@bcf-software.com>
2021-12-16 14:19:55 +0100

    - added RestControllerAdvice

--- src/main/java/com/gogoaren/indarra/serviceweatherapi/controllers/RestControllerHandler.java
+++ src/main/java/com/gogoaren/indarra/serviceweatherapi/controllers/RestControllerHandler.java
@@ -1,31 +1,25 @@
 package com.gogoaren.indarra.serviceweatherapi.controllers;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestControllerAdvice
 public class RestControllerHandler {
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler({MethodArgumentNotValidException.class})
-    public Map<String, Map<String, String>> handleValidationExceptions(
-            MethodArgumentNotValidException ex) {
-        Map<String, Map<String, String>> responseMap = new HashMap<>();
-        Map<String, String> errorList = new HashMap<>();
-        responseMap.put("errors", errorList);
+    private String INCORRECT_REQUEST = "INCORRECT_REQUEST";
 
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errorList.put(fieldName, errorMessage);
-        });
-        return responseMap;
+    @ExceptionHandler(RecordNotFoundException.class)
+    public final ResponseEntity<ErrorResponse> handleCityNotFoundException
+            (RecordNotFoundException ex, WebRequest request) {
+        List<String> details = new ArrayList<>();
+        details.add(ex.getLocalizedMessage());
+        ErrorResponse error = new ErrorResponse(INCORRECT_REQUEST, details);
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 }
