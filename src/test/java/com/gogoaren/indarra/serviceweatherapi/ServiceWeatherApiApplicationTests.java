273fd4f563ad9584dae3646b95c1f54c626da1bc
jablonski.pio <piotr.jablonski@bcf-software.com>
2021-12-20 15:04:44 +0100

    - fix issues

--- src/test/java/com/gogoaren/indarra/serviceweatherapi/ServiceWeatherApiApplicationTests.java
+++ src/test/java/com/gogoaren/indarra/serviceweatherapi/ServiceWeatherApiApplicationTests.java
@@ -3,7 +3,6 @@ package com.gogoaren.indarra.serviceweatherapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ServiceWeatherApiApplicationTests {
 
 	@Test
