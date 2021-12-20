e2dd4fce42151cf5746d707f21b134bf4a3c267b
jablonski.pio <piotr.jablonski@bcf-software.com>
2021-12-20 14:51:24 +0100

    - weather scheduler add job

--- src/main/java/com/gogoaren/indarra/serviceweatherapi/SchedulerQuartz/WeatherJobService.java
+++ src/main/java/com/gogoaren/indarra/serviceweatherapi/SchedulerQuartz/WeatherJobService.java
@@ -1,11 +1,11 @@
 package com.gogoaren.indarra.serviceweatherapi.SchedulerQuartz;
 
-import com.gogoaren.indarra.serviceweatherapi.Scheduler.CronScheduler;
-import com.gogoaren.indarra.serviceweatherapi.utils.RandomCitiesGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+
+import java.util.stream.Collectors;
 
 import static com.gogoaren.indarra.serviceweatherapi.utils.RandomCitiesGenerator.*;
 
@@ -20,9 +20,20 @@ public class WeatherJobService {
     }
 
     @PostConstruct
-    public void runWeatherJob() {
+    public void runWeatherJobForCitiesStartsWithLetterA() {
         final TimerInfo info = new TimerInfo();
-        info.setCronExpression("0 0/20 * ? * *");
+        info.setCronExpression(" 0 5 * ? * *"); // At second :00 of minute :05 of every hour
+        info.setCityNames(getCityList()
+                .stream()
+                .filter(x->x.startsWith("A"))
+                .collect(Collectors.toList()));
+        scheduler.schedule(WeatherJob.class, info);
+    }
+
+    @PostConstruct
+    public void runWeatherJobForAllCities() {
+        final TimerInfo info = new TimerInfo();
+        info.setCronExpression("0 0/20 * ? * *"); // At second :00, every 20 minutes starting at minute :00, of every hour
         info.setCityNames(getCityList());
         scheduler.schedule(WeatherJob.class, info);
     }
